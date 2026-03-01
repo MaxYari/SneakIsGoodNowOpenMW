bd2f56ae520310f1b8e169cc7984e7862a0bcd58
Maksim Eremenko <maksyarik@gmail.com>
2026-03-01 19:35:00 +0100

    version 1.0

---
+++ README.md
@@ -0,0 +1,91 @@
+# Sneak! - Sneak is good now.
+
+An OpenMW mod that makes sneak mechanics playable. Sneak detection is now a visually displayed gradual progress. It loosely follows original sneak detection formulas in a sense that whichever creature was difficult to sneak by in the original will also be difficult to sneak by now. There are many small tweaks to how those formulas work and how they translate into a gradual detection progress, aimed at providing a more fun gameplay experience. This mod attempts to strike a balance between gamified mechanics and """realism""".
+
+Features:
+
+- Gradual visual detection progress instead of instant detection.
+
+- Weapon skill is boosted by 50% while in sneak stance.
+
+- Slight increase of sneak speed (it's 90% of the walk speed now). Due to how sneak animations and footstep sounds work it *might* feel like sneaking is faster than walking now - it is not, footstep sounds are just more frequent in sneaking stance.
+
+- Contrary to vanilla boots don't affect your sneak chances. All in all your equipment has no effect on your sneak.
+
+- Mod works by kicking you out of a sneaking stance when you are detected, so the engine can take over the NPC agression/attack logic, so dont be surprised when you are suddenly forced out of a sneaking stance - this is intended.
+
+
+## Recommendations
+
+Try [Dynamic Reticle](https://www.nexusmods.com/morrowind/mods/56584) they go well together, as it adds some subtle sneak visual effects.
+
+There are also few sneak-related mods out there such as [Burglary Overhaul](https://www.nexusmods.com/morrowind/mods/56965) and [SHOP](https://www.nexusmods.com/morrowind/mods/57747), both mod authors were very helpful and Sneak! will eventually seamlessly work with both of them (but probably it doesnt right now). I haven't personally tested them together myself, but wanted to put them on your radar!
+
+## Installation
+
+NO MATTER WHICH INSTALLATION APPROACH YOU USE you need to **enable `SneakIsGoodNow.omwscripts` and `SneakIsGoodNow.omwaddon` files** inside the `ContentFiles` tab of OpenMW launcher. You are done, have fun!
+
+AND be sure to **enable `Toggle Sneak`** - this option makes it so that you don't have to hold sneak button to sneak, this mod will not work properly without it. Don't remember where that setting is and I'm too lazy to look it up, it's somewhere in the launcher or in game settings.
+
+Install as any other OpenMW mod, if you installed an OpenMW mod before - you know what to do, information below is for those who are new to OpenMW modding.
+
+I'm usually lazy in describing all the ways one can install an OpenMW mod, but this time I'll do it justice! So if you are new to OpenMW mods - there are 3 distinct ways to install any OpenMW mod, here they are from least to most complicated:
+
+Use **ONE OF these methods**
+
+**A) Use Mod Organiser 2 with an OpenMW addon** - it automates installation of OpenMW mods and has Nexus integration - either drag and drop a downloaded mod archive into it or, if you are downloading from Nexus - MO2 will handle the mod download for you as soon as you press the download button. Probably you also need to click on the mod in MO2 after download is done and click install? I haven't used MO2 for a bit since I'm on Linux now, but I highly recommend MO2.
+
+**B) OR Install using an OpenMW launcher** - also quite a convenient method that's easy to use for mods that don't have a BAIN/FOMOD installer (a special option-selection tool bundled with some mods, this one does not use it). First extract the mod archive somewhere so it's now a single mod folder (not a `scripts` folder, but a mod folder containing a `scripts` folder), then in the OpenMW launcher go to `Data Directories` tab and use `Append` button to add the extracted mod folder to a list of Data Directories.
+
+**C) OR Take the contents of this mod's archive and drop them into** a `Data Files` folder inside your Morrowind installation (not OpenMW!).
+
+## For Developers
+
+**Sneak!** provides a minimalist interface exposing current player detection state. You can read about how to use script interfaces [here](https://openmw-zack.readthedocs.io/en/lua_global_new/reference/lua-scripting/overview.html#script-interfaces).
+
+Note that Sneak! mostly only works while player is sneaking, it does not do any detection or line-of-sight checks when player is not sneaking.
+
+Interface is available only on player scripts, it exposes a player detection state:
+
+```Lua
+local ps = I.SneakIsGoodNow.playerState
+-- Available properties:
+ps.isSneaking -- boolean
+ps.detectedByNonAggro -- boolean
+ps.isMoving -- boolean
+ps.isInvisible -- boolean
+ps.chameleon -- 1 - 100 number - strength of chameleon effect on a player
+
+```
+
+Since the mod works by kicking you out of sneak state when you are detected - you can consider player detected when `ps.isSneaking` is false. Unless you are detected only by non-aggressive creatures (player is not booted out of sneak then), then `isSneaking` will still be true while `detectedByNonAggro` will be true.
+
+Furthermore there is a table with few properties that you can modify to affect how detectable player is:
+
+```Lua
+local extraMods = I.SneakIsGoodNow.playerState.extraMods
+extraMods.elusivenessMod = 1.0 -- default value
+extraMods.elusivenessConst = 0 -- default value
+```
+
+`elusivenessMod` is more or less a general modifier of how elusive player is, it's applied before chameleon; adds a flat bonus to overall elusiveness.
+
+`elusivenessConst` is a flat bonus that will be added to player's elusiveness score.
+
+If multiple mods will be altering these values - obviously last one will win, since I don't have a system in place for multiple mods to introduce their own separate modifiers, but it's a fairly niche use case so I'm sure it will be fiiiiiine.
+
+I also haven't tested this API at all, but I'm sure it will be fiiiiiine ;)
+
+## Appreciation
+
+Thanks to [Blurpandra](https://www.nexusmods.com/profile/blurpandra/mods?gameId=100) (Erin Pentecost) for sharing his sneak detection code used in his Burglary Overhaul mod. To [fallchildren](https://gitlab.com/fallchildren) and [choirbug](https://gitlab.com/olyukha) for inspiring me to try this janky aproach to a sneak overhaul - they are currently working on a "Dark Project" mod (you can find it in OpenMW discor in mods section) which is a more ambitious stealth overhaul inspired by Thief series mechanics. 
+
+Check their mods, especially the [footstep sound mod](https://gitlab.com/fallchildren/openmw-footsteps), choirbug made a whole bunch of very immersive amazing sounds for it (to hear them you will need to change the sound backend in the mod's settings).
+
+And also to the entirety of the OpenMW community, my inspiration almost always comes from interacting with OpenMW discord, sometimes we have some differences admittedly, but hey, who doesn't? Love you all :3
+
+## AI use discalimer
+
+To be honest, nowadays everyone seem to be using LLMs to some capacity, so leaving disclaimers like that becomes pointless, but sometimes I still do since I know some people are bothered by the whole AI thing (not without a reason) and will appreciate a disclaimer.
+
+So I mostly used Qwen to help with some coding tasks and with spelling and grammar in this readme, as well as ChatGPT for a research into creature aggression OpenMW source code and porting that to Lua.
---
+++ scripts/MaxYari/SneakIsGoodNow/SneakPlayer.lua
@@ -0,0 +1,420 @@
+
+
+local mp = "scripts/MaxYari/SneakIsGoodNow/"
+DebugLevel = 1
+
+local I = require("openmw.interfaces")
+local types = require("openmw.types")
+local nearby = require("openmw.nearby")
+local core = require("openmw.core")
+local omwself = require("openmw.self")
+local util = require("openmw.util")
+local ui = require('openmw.ui')
+local aux_util = require('openmw_aux.util')
+
+local DEFS = require(mp .. 'scripts/sneak_defs')
+local gutils = require(mp .. 'scripts/gutils')
+local itemutil = require(mp .. "scripts/item_utils")
+local detection = require(mp .. "detection_math")
+local aggression = require(mp .. "aggression_math")
+local DetectionMarker = require(mp .. "Sneak_ui_elements")
+local selfActor = gutils.Actor:new(omwself)
+
+gutils.print("Sneak! E-N-G-A-G-E-D", 0)
+
+local sneakCheckPeriod = 0.33 -- seconds between sneak checks per actor
+local followTargetsCheckPeriod = 2.0 -- seconds between follow target updates per actor
+local losCheckPeriod = 0.2
+local detectionDecreaseRate = 0.25  -- fixed decrease rate per second
+
+-- "ps" stands for "Player State"
+local ps = {
+    isSneaking = false,
+    detectedByNonAggro = false,
+    isMoving = false,
+    isInvisible = false,
+    chameleon = 0
+}
+
+local extraMods = {
+    elusivenessMod = 1.0,
+    elusivenessConst = 0
+}
+
+local modifiedSkill = nil
+local skillMod = 0
+local lastCell = nil
+
+local observerActorStatuses = {}
+local persistantActorStatuses = {}
+
+local interface = {
+    version = 1.0,
+    observerActorStatuses = observerActorStatuses,
+    playerState = ps,
+    extraMods = extraMods
+}
+
+    
+-- "ast" stands for "Actor's Status"
+local function getAst(actor)
+    local ast = persistantActorStatuses[actor.id]
+    if not ast then
+        -- gutils.print("Creating new persistant actor status for " .. actor.recordId)
+        ast = {
+            actor = actor,
+            gactor = gutils.Actor:new(actor),
+            cell = actor.cell,
+            distance = 250,
+            progress = 0.0,
+            successRolls = 0
+        }
+
+        persistantActorStatuses[actor.id] = ast
+    end
+    return ast
+end
+
+local function getAstIfExists(actor)
+    if not persistantActorStatuses[actor.id] then return nil end
+    return getAst(actor)
+end
+
+
+
+
+local function getDetectionVelocity(sneakChance)
+    -- returns a velocity multiplier based on sneak chance
+    -- sneakChance is 0-100
+    -- at 0 sneakChance, velocity is 2.0 (detected quickly)
+    -- at 100 sneakChance, velocity is 0.05 (detection slows to a crawl)
+    local maxDetectDur = 8
+    local minDetectDur = 0.5
+    if not sneakChance then
+        sneakChance = 0
+    end
+
+    local detectDur = util.remap(sneakChance, 0, 100, minDetectDur, maxDetectDur)
+    return 1 / detectDur
+end
+
+local function posAboveActor(actor)
+    local bbox = actor:getBoundingBox()
+    return bbox.center + util.vector3(0, 0, bbox.halfSize.z)
+end
+
+local function getFollowTargets(actor)
+    actor:sendEvent("MaxYariUtil_GetFollowTargets")
+end
+
+local function isFriend(ast)    
+    if not ast.followTargets then return false end
+    if gutils.arrayContains(ast.followTargets, omwself.object) and (not ast.combatTargets or not gutils.arrayContains(ast.combatTargets, omwself.object)) then
+        return true
+    end
+    return false
+end
+
+
+
+
+
+-- Main logic starts here -----------------------------------------------
+-------------------------------------------------------------------------
+local function detectionLogicTick(dt)
+    -- Fetching cell changes and removing actors from other cells
+    local cell = omwself.cell
+    if not lastCell or (lastCell ~= cell and not (lastCell.isExterior and cell.isExterior)) then
+        lastCell = cell
+        for id, ast in pairs(persistantActorStatuses) do
+            if ast.cell ~= cell then 
+                if ast.marker then
+                    ast.marker:disappear()
+                end                
+                persistantActorStatuses[id] = nil
+                observerActorStatuses[id] = nil
+            end
+        end
+    end
+
+    if ps.isSneaking then
+        for _, actor in ipairs(nearby.actors) do
+
+            if actor == omwself.object then goto continue end   
+            
+            local ast = nil   
+            local isDead = types.Actor.isDead(actor)
+            
+            if isDead then
+                ast = getAstIfExists(actor)
+                if ast then
+                    ast.noticing = false
+                    ast.progress = 0
+                    ast.successRolls = 0
+                    ast.isDead = isDead
+                end
+                goto continue
+            end
+            
+            ast = getAst(actor)
+            ast.isDead = isDead
+            
+            local distance = (omwself.position - actor.position):length()
+            ast.distance = distance
+            local noticing = false
+            local sneakChance = 100
+            local isAggro = false
+
+            -- print(ast.actor.recordId, " Aggro range is ", aggroDistance(ast), " while currect detection range is ", detectionRange)
+            
+            -- Detection check! ------------
+            --------------------------------
+            if distance <= detection.detectionRange and not ast.isFriend then
+                if ast.sneakChecker == nil then                        
+                    ast.sneakChecker = gutils.cachedFunction(detection.sneakCheck, sneakCheckPeriod, math.random() * sneakCheckPeriod)
+                end
+                if ast.followTargetsChecker == nil then                    
+                    ast.followTargetsChecker = gutils.cachedFunction(getFollowTargets, followTargetsCheckPeriod, math.random() * followTargetsCheckPeriod)
+                end
+
+                local newSneakChance = nil
+                isNotDetected, newSneakChance = ast.sneakChecker(ast, ps, extraMods)
+                
+                noticing = not isNotDetected
+                if newSneakChance ~= nil then sneakChance = newSneakChance end
+
+                ast.followTargetsChecker(actor)    
+                
+                -- Check if agressive
+                isAggro = aggression.isAggressive(ast, omwself.object)
+                -- print(actor.recordId .. " is " .. (isAggro and "aggressive" or "not aggressive") .. " towards player")
+            end
+            
+            ast.noticing = noticing
+            ast.sneakChance = sneakChance
+            ast.isAggressive = isAggro
+
+            -- Add to observerActorStatuses if noticing OR if there's existing progress that needs to be tracked
+            if ast.noticing or ast.progress > 0 then
+                observerActorStatuses[actor.id] = ast
+            end               
+            
+            ::continue::
+        end
+    end    
+    
+    ps.detectedByNonAggro = false
+    for actorId, ast in pairs(observerActorStatuses) do
+        -- Manage detection progress ----
+        ---------------------------------
+        local detectionVel = getDetectionVelocity(ast.sneakChance)
+
+        if ast.progress == nil then ast.progress = 0.0 end
+        if ast.successRolls == nil then ast.successRolls = 0 end
+        
+        if ast.isDead or not ast.actor:isValid() then
+            ast.progress = 0.0
+            ast.successRolls = 0
+        elseif ast.fightingPlayer then
+            ast.noticing = true
+            ast.progress = 1.0
+        elseif not ast.inLOS then
+            -- Out of LOS: immediate fixed decrease, set successRolls to 5
+            ast.progress = math.max(0.0, ast.progress - dt * detectionDecreaseRate)
+            ast.successRolls = 3
+        elseif ast.noticing then
+            -- Detected: increase with sneak-based velocity, reset counter
+            ast.progress = math.min(1.0, ast.progress + dt * detectionVel)
+            ast.successRolls = 0
+        else
+            -- Not detected: count success rolls
+            ast.successRolls = ast.successRolls + 1
+            if ast.successRolls >= 3 then
+                -- After 3 successes, start decreasing at fixed rate
+                ast.progress = math.max(0.0, ast.progress - dt * detectionDecreaseRate)
+            end
+            -- else: progress stays same
+        end
+
+        -- Send spotted event and break sneak only when detection progress reaches 1.0
+        if ast.progress >= 1.0 then
+            if ast.isAggressive then                
+                ps.isSneaking = false          
+                omwself.controls.sneak = false  -- Break sneak when fully detected 
+            else
+                ps.detectedByNonAggro = true
+            end
+        end
+
+        -- Manage ui markers ------------------
+        ---------------------------------------
+        -- Show markers only when sneaking and detection progress is happening
+        local shouldShowMarker = ps.isSneaking and not ast.isDead and ast.inLOS        
+        if shouldShowMarker then
+            -- If marker doesnt exist but should - make it
+            if not ast.marker then ast.marker = DetectionMarker:new() end
+        elseif ast.marker then
+            -- If it shouldnt exist but does - remove it
+            local isSuccesful = ast.progress >= 1.0
+            ast.marker:disappear(isSuccesful)
+        end
+
+        if ast.marker and ast.marker.destroyed then
+            ast.marker = nil
+        end
+
+        if ast.marker then
+            -- Update the marker's progress and position
+            ast.marker:setProgress(ast.progress)
+            ast.marker:setWorldPos(posAboveActor(ast.actor))
+            ast.marker:setAggressive(ast.isAggressive)
+        end
+
+        -- Final cleanup, if no marker and no progress - remove the status object --
+        ----------------------------------------------------------------------------
+        if (ast.marker == nil) and (ast.progress == 0.0) then
+            observerActorStatuses[actorId] = nil
+        end
+    end
+end
+
+
+local function onUpdate(dt)
+    if dt == 0 then
+        return
+    end   
+
+    -- Fetching locomotion statuses
+    ps.isMoving = selfActor:getCurrentSpeed() > 0 or not selfActor:isOnGround()
+    ps.isSneaking = omwself.controls.sneak
+
+    -- Fetching invisibility status
+    local activeEffects = selfActor:activeEffects()
+    local invisibilityEffect = activeEffects:getEffect(core.magic.EFFECT_TYPE.Invisibility)
+    ps.isInvisible = (invisibilityEffect ~= nil) and (invisibilityEffect.magnitude > 0)
+
+    -- Fetching chameleon effeect
+    local chameleonEffect = activeEffects:getEffect(core.magic.EFFECT_TYPE.Chameleon)    
+    if chameleonEffect ~= nil then
+        ps.chameleon = chameleonEffect.magnitude
+    end    
+    
+    detectionLogicTick(dt)
+
+    -- Update all active tweeners for markers
+    for actorId, ast in pairs(observerActorStatuses) do
+        if ast.marker then ast.marker:updateTweeners(dt) end
+    end
+
+    -- Increase the weapon skill while sneaking
+    local weaponObj = selfActor:getEquipment(types.Actor.EQUIPMENT_SLOT.CarriedRight)
+    local skill = "handtohand"
+    if weaponObj then   
+        skill = itemutil.getSkillTypeForEquipment(weaponObj).id        
+    end           
+    stat = selfActor:getSkillStat(skill)    
+    
+    if ps.isSneaking then
+        if modifiedSkill ~= skill then
+            -- if we switched to a different skill, remove old modifier
+            if modifiedSkill then
+                local oldStat = selfActor:getSkillStat(modifiedSkill)
+                oldStat.modifier = oldStat.modifier - skillMod
+            end
+
+            skillMod = stat.base / 2            
+            modifiedSkill = skill
+            stat.modifier = stat.modifier + skillMod
+        end        
+    else
+        if modifiedSkill then
+            -- remove modifier when not sneaking
+            stat.modifier = stat.modifier - skillMod
+            modifiedSkill = nil
+            skillMod = 0
+        end
+    end
+end
+
+
+
+
+
+-- Event handlers ----------------------------------------------
+----------------------------------------------------------------
+local function onCombatTargetsChanged(e)
+    
+    if e.actor == omwself.object then return end
+    -- print("Combat targets changed for " .. e.actor.recordId)
+
+    local ast = getAst(e.actor)    
+    ast.combatTargets = e.targets    
+    ast.isFriend = isFriend(ast)
+    ast.isDead = types.Actor.isDead(e.actor) 
+
+    if not ast.isDead and gutils.arrayContains(ast.combatTargets, omwself.object) then
+        gutils.print("Player: Combat targets changed for " .. e.actor.recordId, "Player is a target", 1)
+        ast.fightingPlayer = true       
+        ast.isAggressive = true 
+        observerActorStatuses[e.actor.id] = ast
+    else
+        ast.fightingPlayer = false
+    end
+end
+
+local function onGetFollowTargets(e)
+    -- gutils.print("Player: Received follow targets resp from " .. e.actor.recordId, 1)
+    for _, actor in ipairs(e.targets) do
+        gutils.print("Target:",actor.recordId)
+    end
+    if e.actor == omwself.object then return end
+
+    local ast = getAst(e.actor)
+    ast.followTargets = e.targets
+    ast.isFriend = isFriend(ast)
+    -- gutils.print(e.actor.recordId, "Is a friend",ast.isFriend, 1)
+end
+
+local function onReportAttack(e)
+    if e.target == omwself.object then return end
+
+    -- gutils.print("Reported attack by " .. e.attacker.recordId .. " on " .. e.target.recordId)
+    local ast = getAst(e.target)
+    ast.isFriend = isFriend(ast)
+    ast.isDead = types.Actor.isDead(e.target) 
+
+    if e.attacker == omwself.object and not ast.isDead then
+        ast.fightingPlayer = true
+        ast.isAggressive = true        
+        observerActorStatuses[e.target.id] = ast
+    end
+end
+
+local function onSave()
+    return {
+        modifiedSkill = modifiedSkill,
+        skillMod = skillMod
+    }
+end
+
+local function onLoad(data)
+    if data.modifiedSkill then
+        modifiedSkill = data.modifiedSkill
+        skillMod = data.skillMod
+    end
+end
+
+return {    
+    engineHandlers = {
+        onUpdate = onUpdate,
+        onSave = onSave,
+        onLoad = onLoad
+    },
+    eventHandlers = { 
+        OMWMusicCombatTargetsChanged = onCombatTargetsChanged,
+        MaxYariUtil_FollowTargets = onGetFollowTargets,
+        [DEFS.e.ReportAttack] = onReportAttack
+    },
+    interfaceName = DEFS.mod_name,
+    interface = interface
+}
